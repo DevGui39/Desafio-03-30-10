68e1243ca36c26700edb67091596b6ba3137a46e
DevGui39 <guilherme.gomesesteves@gmail.com>
2024-10-30 18:13:15 -0300

    desafio 3

---
+++ index.js
@@ -0,0 +1,13 @@
+
+    let idade = parseInt(prompt("inserir sua idade"))
+    
+    while (idade != "sair") {
+        
+        alert("Essa é sua idade " + idade)
+
+        idade = prompt("insira outra idade")
+
+    }
+
+
+   
